af6505628422188094d59f4836e9399a0e56c314
Ayaaz <ayaazansari3052@gmail.com>
2021-09-25 17:40:24 +0530

    Dagger Hilt Setup

---
+++ app/src/main/java/com/example/runningapp/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.runningapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.runningapp.db.RunningDatabase
+import com.example.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+            @ApplicationContext app: Context
+    ): RunningDatabase {
+        return Room.databaseBuilder(
+                app,
+                RunningDatabase::class.java,
+                RUNNING_DATABASE_NAME
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+}
---
+++ app/src/main/java/com/example/runningapp/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.runningapp.other
+
+object Constants {
+    const val RUNNING_DATABASE_NAME ="Running_db"
+}
