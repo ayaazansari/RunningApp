e8f0502d2b2630a74e72439ae860c8298c04cd11
Ayaaz <ayaazansari3052@gmail.com>
2021-09-26 14:19:40 +0530

    Setting up Navigation

--- app/src/main/java/com/example/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningapp/ui/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.example.runningapp.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.runningapp.R
 import com.example.runningapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDao:RunDAO
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+                .addOnDestinationChangedListener { _, destination, _ ->
+                    when(destination.id) {
+                        R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                            bottomNavigationView.visibility = View.VISIBLE
+                        else -> bottomNavigationView.visibility = View.GONE
+                    }
+                }
+    }
 }
