901353550cd72efc47035a88e873281915d85bf8
Ayaaz <ayaazansari3052@gmail.com>
2021-09-26 22:08:56 +0530

    Total Run Statistics & SettingsFragment

--- app/src/main/java/com/example/runningapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/example/runningapp/ui/viewmodels/StatisticsViewModel.kt
@@ -7,4 +7,10 @@ import com.example.runningapp.repositories.MainRepository
 class StatisticsViewModel @ViewModelInject constructor(
         val mainRepository: MainRepository
 ): ViewModel() {
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
 }
