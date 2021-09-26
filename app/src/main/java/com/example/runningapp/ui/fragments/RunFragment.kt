eef459be71618911cc77ddd9e31c16d31f24a173
Ayaaz <ayaazansari3052@gmail.com>
2021-09-26 21:46:01 +0530

    Showing Runs in the RecyclerView

--- app/src/main/java/com/example/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/RunFragment.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.runningapp.R
+import com.example.runningapp.adapters.RunAdapter
 import com.example.runningapp.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
 import com.example.runningapp.other.TrackingUtility
 import com.example.runningapp.ui.viewmodels.MainViewModel
@@ -22,13 +25,25 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
 
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var runAdapter: RunAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         requestPermissions()
+        setupRecyclerView()
+
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            runAdapter.submitList(it)
+        })
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+    private fun setupRecyclerView() = rvRuns.apply {
+        runAdapter = RunAdapter()
+        adapter = runAdapter
+        layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun requestPermissions() {
--- app/src/main/java/com/example/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/runningapp/ui/viewmodels/MainViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.launch
 class MainViewModel @ViewModelInject constructor(
         val mainRepository: MainRepository
 ): ViewModel() {
-
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
     fun insertRun(run: Run) = viewModelScope.launch {
         mainRepository.insertRun(run)
     }
