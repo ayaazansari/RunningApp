ed825763b3b35da310e6289ab71d57babcc00e3c
Ayaaz <ayaazansari3052@gmail.com>
2021-09-26 20:21:10 +0530

    Handling location permission

--- app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,12 +1,22 @@
 package com.example.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningapp.R
 import com.example.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment:Fragment(R.layout.fragment_tracking) {
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+    }
+
 }
